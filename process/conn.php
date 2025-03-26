3ae64ede0b736e75231e3df24d8ed14ab661e4b8
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-03-26 15:22:36 -0300

    Conectando o banco de dados a aplicação

---
+++ process/conn.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+$user = 'root';
+$pass = 'adminroot';
+$host = 'localhost';
+$db = 'pizzaria';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    //code...
+} catch (PDOException $e) {
+    print "Erro: " . $e->getMessage() . "<br>";
+    die();
+    
+}
+
+?>
