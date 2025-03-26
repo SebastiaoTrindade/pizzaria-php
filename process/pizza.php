39cf51c708c7ffad1a7fba296c4fd86cd7d2691e
Sebastiao Trindade <sebatrindade@hotmail.com>
2025-03-26 16:29:07 -0300

    Resgatando dados para a página index

--- process/pizza.php
+++ process/pizza.php
@@ -1,4 +1,24 @@
 <?php
     include_once ('conn.php');
     
+    $method = $_SERVER['REQUEST_METHOD'];
+
+    // Resgate dos dados e montagem do pedido
+    if ($method === 'GET') {
+
+        $bordasQuery = $conn->query("SELECT * FROM bordas");
+        $bordas = $bordasQuery->fetchAll(PDO::FETCH_ASSOC);
+        
+
+        $massasQuery = $conn->query("SELECT * FROM massas");
+        $massas = $massasQuery->fetchAll(PDO::FETCH_ASSOC);
+
+        $saboresQuery = $conn->query("SELECT * FROM sabores");
+        $sabores = $saboresQuery->fetchAll(PDO::FETCH_ASSOC);
+
+        
+
+    }else if ($method === 'POST') {
+
+    }
 ?>
